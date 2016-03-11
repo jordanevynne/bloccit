ee6368e249f3016b5a8c9dedaf7b8e83f29e53fa
Jordan Marcus <jordan.e.marcus@gmail.com>
2016-03-10 21:20:59 -0800

    Assignment 32 added new docs

---
+++ spec/controllers/advertisement_controller_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+include RandomData
+
+RSpec.describe AdvertisementsController, type: :controller do
+  let(:my_ad) { Advertisement.create!(id: 1, title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 29) }
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+  it "assigns [my_ad] to @advertisements" do
+    get :index
+    expect(assigns(:advertisements)).to eq([my_ad])
+  end
+end
+
+  describe "GET #show" do
+    it "returns http success" do
+      get :show, {id: my_ad.id}
+      expect(response).to have_http_status(:success)
+    end
+
+  it "renders #show" do
+    get :show, {id: my_ad.id}
+    expect(response).to render_template :show
+  end
+
+  it "assigns my_ad to @advertisement" do
+    get :show, {id: my_ad.id}
+    expect(assigns(:advertisement)). to eq(my_ad)
+  end
+end
+
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+
+  describe "GET #create" do
+    it "returns http success" do
+      get :create
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
