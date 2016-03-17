c11e19b696827a4da7b908d4ea87504462c3c536
Jordan Marcus <jordan.e.marcus@gmail.com>
2016-03-17 01:19:58 -0700

    Checkpoint 39 completed

--- lib/random_data.rb
+++ lib/random_data.rb
@@ -1,10 +1,20 @@
 module RandomData
 
+  def self.random_name
+    first_name = random_word.capitalize
+    last_name = random_word.capitalize
+    "#{first_name} #{last_name}"
+  end
+
+  def self.random_email
+    "#{random_word}@#{random_word}.#{random_word}"
+  end
+
   def self.random_paragraph
     sentences = []
     rand(4..6).times do
-      sentences << random_sentence
-    end
+    sentences << random_sentence
+  end
 
     sentences.join(" ")
   end
@@ -12,8 +22,8 @@ module RandomData
   def self.random_sentence
     strings = []
     rand(3..8).times do
-      strings << random_word
-    end
+    strings << random_word
+  end
 
     sentence = strings.join(" ")
     sentence.capitalize << "."
