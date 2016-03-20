3761a1029084ad0e7ae8072dbead61e3e2eae315
Jordan Marcus <jordan.e.marcus@gmail.com>
2016-03-19 19:07:54 -0700

    Checkpoint 41 completed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe Post, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
 
+  it { is_expected.to have_many(:comments) }
+
   it { is_expected.to belong_to(:topic) }
   it { is_expected.to belong_to(:user) }
 
@@ -12,7 +14,7 @@ RSpec.describe Post, type: :model do
   it { is_expected.to validate_presence_of(:body) }
   it { is_expected.to validate_presence_of(:topic) }
   it { is_expected.to validate_presence_of(:user) }
-  
+
   it { is_expected.to validate_length_of(:title).is_at_least(5) }
   it { is_expected.to validate_length_of(:body).is_at_least(20) }
 
