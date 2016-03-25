03472455fff9e334502354c855fd8b75c0ddcb12
Jordan Marcus <jordan.e.marcus@gmail.com>
2016-03-24 22:17:47 -0700

    Assignment 43 completed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -58,6 +58,22 @@ RSpec.describe Post, type: :model do
       end
     end
 
+    describe "#create_vote" do
+      it "creates a new vote for post" do
+        expect( post.up_votes ).to eq(1)
+      end
+
+      it "calls #create_vote when a post is created" do
+        post = topic.posts.new(title: RandomData.random_sentence, body: RandomData.random_sentence)
+        expect(post).to receive(:create_vote)
+        post.save
+      end
+
+      it "associates the vote with the owner of the post" do
+        expect(post.votes.first.user).to eq(post.user)
+      end
+    end
+
     describe "#update_rank" do
       it "calculates the correct rank" do
         post.update_rank
