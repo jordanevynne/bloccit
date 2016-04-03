8077cd501cb51bbae21e8dbc2cdcfd5f771a62f8
Jordan Marcus <jordan.e.marcus@gmail.com>
2016-04-02 19:14:20 -0700

    completed 1-4

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,8 @@ get 'about' => 'welcome#about'
     namespace :v1 do
       resources :users, only: [:index, :show]
       resources :topics, only: [:index, :show]
+      resources :posts, only: [:index, :show]
+      resources :comments, only: [:index, :show]
     end
   end
 end
